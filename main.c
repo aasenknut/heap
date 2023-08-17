afc3a867df33739df9e464bbf2c6c940ac548a8b
Knut <aasenknut@gmail.com>
2023-08-17 11:03:45 +0200

    s

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define CAP 12345
+
+struct Heap {
+    float *arr;
+    size_t len;
+    size_t cap;
+};
+
+struct Heap heapify(float *arr) {
+}
+
+int main() {
+    malloc(CAP * sizeof(float))
+}
