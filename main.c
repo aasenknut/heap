0459191eb0028497b78ad3b36306891b08e8dd7e
aasenknut <aasenknut@gmail.com>
2023-08-25 14:20:46 +0200

    heapifying your array

--- main.c
+++ main.c
@@ -6,15 +6,72 @@
 
 #define CAP 12345
 
-struct Heap {
+struct MinHeap {
     float *arr;
     size_t len;
     size_t cap;
 };
 
-struct Heap heapify(float *arr) {
+
+void push(struct MinHeap *h) {
+}
+
+float pop(struct MinHeap *h) {
+    float x = h->arr[0];
+    h->arr[0] = h->arr[h->len-1];
+    h->len--;
+
+    size_t idx = 0;
+    size_t smallest;
+    float parenIdx;
+    float lIdx;
+    float rIdx;
+    
+    while (1) {
+        if (idx < 0) {
+            break;
+        }
+        smallest = idx;
+        parenIdx = idx / 2;
+        lIdx = idx * 2 + 1;
+        rIdx = idx * 2 + 2;
+        if (lIdx < h->len) && (h->arr[lIdx] < h->arr[smallest]) {
+            smallest = lIdx
+        }
+        if (rIdx < h->len) && (h->arr[rIdx] < h->arr[smallest]) {
+            smallest = rIdx
+        }
+    }
+
+    return x;
+}
+
+void arrange(struct MinHeap *h) {
+}
+
+
+struct MinHeap heapify(float *arr, size_t arrLen) {
+    struct MinHeap heap;
+    heap.len = arrLen;
+    heap.cap = CAP;
+    heap.arr = (float *)malloc(sizeof(float) * CAP);
+    if (heap.arr == NULL) {
+        return heap; // Return an error code
+    }
+    for (size_t j = 0; j < arrLen; j++) {
+        heap.arr[j] = arr[j];
+    }
+    return heap;
 }
 
 int main() {
-    malloc(CAP * sizeof(float))
+    struct MinHeap heap;
+    float arr[5] = {1.5, 3.2, 1.1, 4.5, 7.9};
+    heap = heapify(arr, 5);
+    if (heap.arr == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return 1;
+    }
+    free(heap.arr);
+    return 0;
 }
